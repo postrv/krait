57e671ae0607ab30f57796064a8477539d3213e0
postrv <71533421+postrv@users.noreply.github.com>
2026-05-14 23:09:57 +0100

    Harden CI NIF quality gates

--- test/release/immutable_guard_workflow_test.exs
+++ test/release/immutable_guard_workflow_test.exs
@@ -35,4 +35,19 @@ defmodule Krait.Release.ImmutableGuardWorkflowTest do
     assert manifest =~ "\n.github/\n"
     refute manifest =~ ".github/workflows/"
   end
+
+  test "elixir jobs do not cache _build artifacts" do
+    workflow = File.read!(@workflow)
+
+    assert workflow =~ "Cache Elixir deps"
+    refute workflow =~ "Cache Elixir deps + _build"
+    refute workflow =~ "deps\n            _build"
+  end
+
+  test "quality gates smoke-test the rust nif load path" do
+    workflow = File.read!(@workflow)
+
+    assert workflow =~ "Validate Rust NIF load"
+    assert workflow =~ "Krait.Analyzer.Nif.quick_validate"
+  end
 end
